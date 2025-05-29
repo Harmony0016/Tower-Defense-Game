1d541752d624848d63990e7191bbee744171fd6e
maste <maste@10.78.20.110>
2025-05-29 14:01:55 -0400

    Here are some tiles

--- Tower Defense Game/src/application/Client.java
+++ Tower Defense Game/src/application/Client.java
@@ -2,20 +2,37 @@ package application;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Client extends Application {
 
+	int [][] tiles = new int [120][256]; //creates grid on the plane
+	
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+    	
+    	GridPane map = new GridPane();
+    	
+    	getTiles(map);
+    	
+    	Scene scene = new Scene(map);
+    	primaryStage.setScene(scene);
+    	primaryStage.show();
+    	
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    private void getTiles(GridPane map) {
+    	for (int i = 0; i < 42; i++) { //every section for the tiles 
+    		for (int j = 0; j < 24; j++) {
+    			int type = tiles[i][j];
+    			Tile grid = new Tile(type); //creates the tile object
+    			map.add(grid,i,j);
+    		}
+    	}
     }
 }
--- Tower Defense Game/src/application/Tile.java
+++ Tower Defense Game/src/application/Tile.java
@@ -4,4 +4,9 @@ import javafx.scene.layout.Pane;
 
 public class Tile extends Pane {
 
+	public Tile(int type) {
+		this.setStyle("-fx-border-color: black"); //42,24
+		this.setPrefSize(40, 40);
+	}
+	
 }
